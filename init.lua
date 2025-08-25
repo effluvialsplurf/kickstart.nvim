cd5ed880c361fe239e1498ac9708f22c91762813
oliverArchBox <eesowrk@gmail.com>
2025-08-24 23:45:30 -0400

    adding nvim-dadbod and removing dbee

--- init.lua
+++ init.lua
@@ -725,6 +725,10 @@ require('lazy').setup({
         default = { 'lsp', 'path', 'snippets', 'lazydev' },
         providers = {
           lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+          dadbod = { name = 'Dadbod', module = 'vim_dadbod_completion.blink' },
+        },
+        per_filetype = {
+          sql = { 'snippets', 'dadbod', 'buffer' },
         },
       },
 
